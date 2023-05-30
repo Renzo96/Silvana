d13afeba5ce426ec2220cfc8157b6f7b8cf7a58d
Renzo Sosa <67750743+Renzo96@users.noreply.github.com>
2023-05-29 21:18:04 -0300

    jkj

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ from fastapi.responses import PlainTextResponse, FileResponse, HTMLResponse, JSO
 from fastapi import Response, Depends, HTTPException, FastAPI, Form
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 import pandas as pd
+import os
 import mysql.connector
 from starlette import responses
-import os
 
 app = FastAPI()
 security = HTTPBasic()
 
-users = {
-    "Admin": "password123",
-    "Silvana": "password456"
-}
+db_host = os.getenv("DB_HOST")
+db_port = int(os.getenv("DB_PORT"))
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
+
 
 @app.get("/")
 def home():
@@ -25,29 +27,49 @@ sessions = {}
 
 @app.post("/login")
 async def login(credentials: HTTPBasicCredentials = Depends(security)):
+    # Estableciendo conexión con la base de datos
+    conexion = mysql.connector.connect(
+        host=db_host,
+        port=db_port,
+        user=db_user,
+        password=db_password,
+        database=db_name
+    )
+    cursor = conexion.cursor()
+
+    query = 'SELECT * FROM usuarios'
+    cursor.execute(query)
+
+    df = pd.read_sql_query(query, conexion)
+
+    conexion.commit()
+    conexion.close()
+
     username = credentials.username
     password = credentials.password
 
-    # Checking valid username and password
-    if username not in users or users[username] != password:
+    # Chequeo de usuario y contraseña válido
+    if username in df['usuario'].values and password == df.loc[df['usuario'] == username, 'contraseña'].values[0]:
+        sessions[username] = True
+        return {"Message": "Login successful"}
+    else:
         raise HTTPException(status_code=401, detail="Incorrect username or password")
 
-    sessions[username] = True
-    return {"Message": "Login successful"}
+
 
 @app.get("/network_map")
-def network_map(credentials: HTTPBasicCredentials = Depends(security)):
+def network_map(request: Request, credentials: HTTPBasicCredentials = Depends(security)):
     username = credentials.username
-
+    
     if username not in sessions:
-        raise HTTPException(status_code=401, detail="You need to log in to execute this request.")
-
-    # Fragmento para poder descargar info en txt
+        return {"Message": "You need to log in to execute this request."}
+    
+    #Fragmento para poder descargar info en txt
     interfaz_info = get_interface_info()
     interfaz_info_str = "\n".join([f"{k}: {v}" for k, v in interfaz_info.items()])
     response = Response(content=interfaz_info_str, media_type="text")
     response.headers["Content-Disposition"] = "attachment; filename=mapa_red.txt"
-
+    
     return response
 
 
@@ -140,4 +162,4 @@ async def logout(credentials: HTTPBasicCredentials = Depends(security)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0" , port=3306)
